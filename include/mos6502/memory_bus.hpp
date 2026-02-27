278463a3cb8e91848a46b6675e9883c2a508fa6d
Ayden Kranz <ayden.kranz@gmail.com>
2026-02-27 16:21:35 -0500

    Pivot approach again. Leaning more heavily into microcode operations

--- include/mos6502/memory_bus.hpp
+++ include/mos6502/memory_bus.hpp
@@ -1,31 +1,56 @@
 #ifndef MOS6502_MEMORYBUS_HEADER
 #define MOS6502_MEMORYBUS_HEADER
 
-#include "chip.hpp"
-
+#include <type_traits>
 #include <cstdint>
-#include <array>
+#include <tuple>
 
-template <std::size_t max_address>
-class MOS6502_MemoryBus{
-public:
-    MOS6502_MemoryBus(){}
-    MOS6502_MemoryBus(const std::array<std::uint8_t, max_address>& mi) : memory(mi){}
+namespace libemu::mos6502{
 
-    void write(MOS6502_Chip<MOS6502_MemoryBus>* chip, std::uint16_t addr, std::uint8_t val){
-        data = val;
-        if (addr >= max_address) return;
-        memory[addr] = val;
-    }
-    std::uint8_t read(MOS6502_Chip<MOS6502_MemoryBus>* chip, std::uint16_t addr){
-        if (addr >= max_address) return data;
-        data = memory[addr];
-        return data;
-    }
+namespace impl{
+    template<typename>
+    struct member_function_traits;
 
-private:
-    std::array<std::uint8_t, max_address> memory;
-    std::uint8_t data;
-};
+    template<typename R, typename C, typename... Args>
+    struct member_function_traits<R(C::*)(Args...)> {
+        using return_type = R;
+        using args_tuple  = std::tuple<Args...>;
+    };
+}
+
+template <class T>
+concept memory_bus = requires{
+    requires requires {
+        typename impl::member_function_traits<decltype(&T::write)>;
+        typename impl::member_function_traits<decltype(&T::read)>;
+    };
+}
+&&
+[]<class Bus>(){
+    using write_traits = impl::member_function_traits<decltype(&Bus::write)>;
+    using read_traits  = impl::member_function_traits<decltype(&Bus::read)>;
+
+    using write_args = typename write_traits::args_tuple;
+    using read_args  = typename read_traits::args_tuple;
+
+    using addr_type = std::tuple_element_t<0, write_args>;
+    using data_type = std::tuple_element_t<1, write_args>;
+    using read_addr = std::tuple_element_t<0, read_args>;
+
+    return
+        std::is_unsigned_v<addr_type> &&
+        std::is_unsigned_v<data_type> &&
+        std::is_unsigned_v<read_addr> &&
+
+        sizeof(addr_type) >= sizeof(std::uint16_t) &&
+        sizeof(read_addr) >= sizeof(std::uint16_t) &&
+        sizeof(data_type) >= sizeof(std::uint8_t)  &&
+
+        std::same_as<typename write_traits::return_type, void> &&
+        std::same_as<typename read_traits::return_type, std::uint8_t>;
+
+}.template operator()<T>();
+
+}
 
 #endif
